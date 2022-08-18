a7977cb5a23113104bd7fbe94d72da1bc1d52951
Sabrina Silva <lima.sbrn@gmail.com>
2022-08-18 20:01:37 -0300

    Add images/media queries

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -7,16 +7,42 @@ import moonIcon from "../icons/moon-icon.svg";
 import { PresentationWrapper } from "~/components/presentation";
 import { SectionWrapper } from "~/components/section";
 import { Skill } from "~/components/skill";
+import linha1Right from "../images/1r-line.svg"
+import linha2Right from "../images/2r-line.svg"
+import linhaCurve from "../images/curve-line.svg"
+import x1Left from "../images/x-1.svg"
+import x2Right from "../images/x-2.svg"
+import blackCircleTop from "../images/cicle-top.svg"
+import blackCircle from "../images/r-b-cicle.svg"
+import eye1 from "../images/eye-1.svg"
+import eye2 from "../images/eye-2.svg"
+import x3Right from "../images/x-3.svg"
+import blackCircleLeft from "../images/circle-left.svg"
+import footerLine from "../images/line_footer.svg"
+
 
 export default function Index() {
   return (
     <div>
       <div className="btn-section">
+        <div>
+          <img className="linha1" src={linha1Right}/>
+      
+        </div>
         <button className="mode_btn hover_item">
           <img src={moonIcon} />
         </button>
       </div>
       <PresentationWrapper>
+      <img className="linha2" src={linha2Right}/>
+      <img className="linha_curve" src={linhaCurve}/>
+      <img className="x1_left" src={x1Left}/>
+      <img className="x2_right" src={x2Right}/>
+      <img className="black_circle_top" src={blackCircleTop}/>
+      <img className="black_circle" src={blackCircle}/>
+      <img className="eye_1" src={eye1}/>
+      <img className="circle_left" src={blackCircleLeft} />
+
         <div className="presentation centralize">
           <img src={logo} />
           <h3>Front - end Jr & Ilustradora</h3>
@@ -70,6 +96,8 @@ export default function Index() {
       </SectionWrapper>
 
       <SectionWrapper>
+        <img className="eye_2" src={eye2} />
+        <img className="x3_right" src={x3Right} />
         <div className="container">
           <h2>Contatos</h2>
           <p>Me mande um alô!</p>
@@ -86,8 +114,9 @@ export default function Index() {
         </div>
       </SectionWrapper>
 
-      <footer>
-        <img src={littleLogo} />
+      <footer className="footer_container">
+        <img src={footerLine} className="footer_line"/>
+        <img src={littleLogo} className="footer_logo" />
       </footer>
     </div>
   );
